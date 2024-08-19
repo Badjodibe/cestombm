23105e42b48867a7ab4c5be1619cc04289b53525
Mabakal <badjokoffi1@gmail.com>
2024-08-19 14:31:50 +0100

    update

---
+++ src/main/java/com/cestombm/cestombm/CestombmApplication.java
@@ -0,0 +1,13 @@
+package com.cestombm.cestombm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CestombmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CestombmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cestombm/cestombm/CestombmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cestombm.cestombm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CestombmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
